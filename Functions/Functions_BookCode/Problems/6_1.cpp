7769a947776f224796fbd6aa18623779eb6c5e96
Beyza Mercan <beyzamercanse@gmail.com>
2023-06-24 20:36:50 +0300

    initial commit

---
+++ Functions/Functions_BookCode/Problems/6_1.cpp
@@ -0,0 +1,21 @@
+/*
+1st problem
+Write a program that asks the user to enter item's wholesale cost and its markup
+percentage. It should then display the item's retail price.  For example:
+
+	* If an item's wholesale cost is 5.00 and its markup percentage is 100%. then
+	  the item's retail price is 10.00.
+	* If an item's wholesale cost is 5.00 and its markup percentage is 50%. then
+	  the item's retail price is 7.50.
+
+The program should have a function named calculateRetail that receives the wholesale
+cost and the markup percentage as arguments, and returns the retail price of the item.
+
+	  Input Validation: Do not accept negative values ofr either the wholesale cost
+	  of the item or the percent markup.
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
