7fbffa9eccac5e2acd619d8284888fc2d62ccc46
Beyza Mercan <beyzamercanse@gmail.com>
2023-08-12 20:46:51 +0300

    commit all

---
+++ Literals and Constants/Bootcamp1/main2.cpp
@@ -0,0 +1,31 @@
+// CONSTANTS
+
+#include <iostream>
+
+
+
+int main(){
+
+    const int age =34;
+    const float height =1.67f;
+
+    //age = 55; // Can't modify
+    //height = 1.8f;
+
+
+    int years = 3 * age;
+
+    std::cout << "age : " << age << std::endl;
+    std::cout << "height : " << height << std::endl;
+    std::cout << "years : " << years << std::endl;
+   
+    return 0;
+}
+
+/*
+
+age : 34
+height : 1.67
+years : 102
+
+*/
